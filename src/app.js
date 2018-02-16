85fe188e7609b3db2590fd1efff4eb4b7310df08
Joshua Rippon <joshua@pomerolpartners.com>
2018-02-16 13:51:29 +0000

    Add 404

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -16,11 +16,13 @@ const AddExpensePage = () => (
     </div>
 )
 
+
 const EditExpensePage = () => (
     <div>
         This is from my edit expense component.
     </div>
 );
+
 
 const HelpPage = () => (
     <div>
@@ -28,14 +30,21 @@ const HelpPage = () => (
     </div>
 );
 
+const NotFoundPage = () => (
+    <div>
+        404!
+    </div>
+);
+
 const routes = (
     <BrowserRouter>
-        <div>
+        <Switch>
             <Route path="/" component={ExpenseDashboardPage} exact={true} />
             <Route path="/create" component={AddExpensePage} />
             <Route path="/edit" component={EditExpensePage} />
             <Route path="/help" component={HelpPage} />
-        </div>
+            <Route component={NotFoundPage} />
+        </Switch>
     </BrowserRouter>
 );
 
